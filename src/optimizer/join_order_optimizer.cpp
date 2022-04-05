857f7429649f1939611b8e46d84ea346468af9b3
Tmonster <tom@ebergen.com>
2022-04-05 15:21:17 +0200

    Squashed commit of the following: "Basically adding code to show cardinalities of joins in the trees"
    commit 429eb91997995a471c3cf3420ffd7bc28343c041
    Author: Tmonster <tom@ebergen.com>
    Date:   Tue Apr 5 14:59:49 2022 +0200
    
        ok, added cardinality counts when printing join tree (not everywhere though)

--- src/optimizer/join_order_optimizer.cpp
+++ src/optimizer/join_order_optimizer.cpp
@@ -745,7 +745,6 @@ JoinOrderOptimizer::GenerateJoins(vector<unique_ptr<LogicalOperator>> &extracted
 		result_operator = move(extracted_relations[node->set->relations[0]]);
 	}
 	result_operator->estimated_cardinality = node->cardinality;
-
 	// check if we should do a pushdown on this node
 	// basically, any remaining filter that is a subset of the current relation will no longer be used in joins
 	// hence we should push it here
@@ -859,7 +858,7 @@ unique_ptr<LogicalOperator> JoinOrderOptimizer::RewritePlan(unique_ptr<LogicalOp
 		op = op->children[0].get();
 	}
 	// have to replace at this node
-//	std::cout << join_tree.second->ToString() << std::endl;
+	std::cout << join_tree.second->ToString() << std::endl;
 	parent->children[0] = move(join_tree.second);
 	return plan;
 }
