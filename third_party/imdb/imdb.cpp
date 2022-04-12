1a61f587807813a9668628d7b38b0b24b96c2659
Tmonster <tom@ebergen.com>
2022-04-12 15:35:41 +0200

    benchmark runner works better now

--- third_party/imdb/imdb.cpp
+++ third_party/imdb/imdb.cpp
@@ -1,25 +1,31 @@
 #include "include/imdb.hpp"
 #include "imdb_constants.hpp"
 #include "duckdb/common/file_system.hpp"
-
+#include <iostream>
 
 using namespace duckdb;
 using namespace std;
 
 namespace imdb {
 
+void print_file(const string &file_name, bool _exists) {
+	std::cout << file_name << std::endl;
+}
+
 void dbgen(DuckDB &db) {
 	Connection con(db);
 	con.Query("BEGIN TRANSACTION");
+	auto file_system = FileSystem::CreateLocal();
+	file_system->SetWorkingDirectory("third_party/imdb/data/");
 	for (int t = 0; t < IMDB_TABLE_COUNT; t++) {
 		con.Query(IMDB_TABLE_DDL[t]);
 		string table_name = string(IMDB_TABLE_NAMES[t]);
-		string data_file_name = "third_party/imdb/data/"+table_name+".csv.gz";
-		auto file_system = FileSystem::CreateLocal();
+		string data_file_name = table_name+".csv";
 		if (!file_system->FileExists(data_file_name)) {
-			throw Exception("IMDB data file missing, try `make imdb` to download.");
+			throw Exception("IMDB data file missing, could not find " + data_file_name + ". try `make imdb` to download.");
 		}
 		con.Query("COPY "+table_name+" FROM '"+data_file_name+"' DELIMITER ',' ESCAPE '\\';");
+		std::cout << "COPY "+table_name+" FROM '"+data_file_name+"' DELIMITER ',' ESCAPE '\\';" << std::endl;
 	}
 	con.Query("COMMIT");
 }
