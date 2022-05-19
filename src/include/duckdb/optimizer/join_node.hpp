1f95df647f8b118dda723a1d7a42c9fb243acc79
Tmonster <tom@ebergen.com>
2022-05-19 09:36:59 +0200

    remove unnecessary header file

--- src/include/duckdb/optimizer/join_node.hpp
+++ src/include/duckdb/optimizer/join_node.hpp
@@ -13,7 +13,6 @@
 #include "duckdb/optimizer/join_order/query_graph.hpp"
 #include "duckdb/optimizer/join_order/join_relation.hpp"
 #include "duckdb/parser/expression_map.hpp"
-//#include "duckdb/planner/logical_operator.hpp"
 #include "duckdb/planner/logical_operator_visitor.hpp"
 
 #include <map>
