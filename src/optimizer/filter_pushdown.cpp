1da0ccf969a3c9e61f2414c327e8118f565c50ea
Tom Ebergen <tom@ebergen.com>
2022-12-13 11:06:17 +0100

    make format fix

--- src/optimizer/filter_pushdown.cpp
+++ src/optimizer/filter_pushdown.cpp
@@ -9,8 +9,7 @@ namespace duckdb {
 
 using Filter = FilterPushdown::Filter;
 
-FilterPushdown::FilterPushdown(Optimizer &optimizer)
-    : optimizer(optimizer), combiner(optimizer.context) {
+FilterPushdown::FilterPushdown(Optimizer &optimizer) : optimizer(optimizer), combiner(optimizer.context) {
 }
 
 unique_ptr<LogicalOperator> FilterPushdown::Rewrite(unique_ptr<LogicalOperator> op) {
