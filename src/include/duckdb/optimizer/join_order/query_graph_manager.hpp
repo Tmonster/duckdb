567cbf1190a54ee9467b41b6c3d63aa0ae78d7c9
Tmonster <tom@ebergen.com>
2024-07-24 11:00:48 +0200

    almost ready to reorder left joins. tpcds still has failures

--- src/include/duckdb/optimizer/join_order/query_graph_manager.hpp
+++ src/include/duckdb/optimizer/join_order/query_graph_manager.hpp
@@ -108,7 +108,7 @@ private:
 
 	void GetColumnBinding(Expression &expression, ColumnBinding &binding);
 
-	void CreateHyperGraphEdges();
+	void CreateHyperGraphEdges(unordered_map<idx_t, unordered_set<idx_t>> left_join_right_child_relation_required_relations_for_join);
 
 	GenerateJoinRelation GenerateJoins(vector<unique_ptr<LogicalOperator>> &extracted_relations, JoinRelationSet &set);
 };
