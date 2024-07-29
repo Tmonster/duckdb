42ad8f65aa105bfb8539cf0c3e28b62c0647308f
Tmonster <tom@ebergen.com>
2024-07-29 18:05:51 +0200

    remove 3% slowness for plan cost runner

--- scripts/plan_cost_runner.py
+++ scripts/plan_cost_runner.py
@@ -77,7 +77,8 @@ class PlanCost:
         # we know the total cardinality is either the same or higher and the build side has not increased
         # in this case fall back to the timing. It's possible that even if the probe side is higher
         # since the tuples are in flight, the plan executes faster
-        return self.time > other.time * 1.03
+        return False
+        # return self.time > other.time * 1.03
 
     def __lt__(self, other):
         if self == other:
@@ -93,11 +94,22 @@ def op_inspect(op) -> PlanCost:
     if 'Query' in op:
         cost.time = op['operator_timing']
     if 'name' in op and op['name'] == 'HASH_JOIN' and not op['extra_info'].startswith('MARK'):
-        cost.total = op['operator_cardinality']
-        if 'operator_cardinality' in op['children'][0]:
-            cost.probe_side += op['children'][0]['operator_cardinality']
-        if 'operator_cardinality' in op['children'][1]:
-            cost.build_side += op['children'][1]['operator_cardinality']
+        if 'operator_cardinality' in op.keys():
+            cost.total = op['operator_cardinality']
+            if 'operator_cardinality' in op['children'][0]:
+                cost.probe_side += op['children'][0]['operator_cardinality']
+            if 'operator_cardinality' in op['children'][1]:
+                cost.build_side += op['children'][1]['operator_cardinality']
+        elif 'cardinality' in op.keys():
+            cost.total = op['cardinality']
+            if 'cardinality' in op['children'][0]:
+                cost.probe_side += op['children'][0]['cardinality']
+            if 'cardinality' in op['children'][1]:
+                cost.build_side += op['children'][1]['cardinality']
+        else:
+            print("cardinlaity and operator cardinality are both not in the op")
+            exit(1)
+
         left_cost = op_inspect(op['children'][0])
         right_cost = op_inspect(op['children'][1])
         cost.probe_side += left_cost.probe_side + right_cost.probe_side
