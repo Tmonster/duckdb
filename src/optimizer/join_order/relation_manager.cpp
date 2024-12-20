46e388d2f5ad74bebc543ac308a85ca8ba59636c
Tmonster <tom@ebergen.com>
2024-12-20 11:34:02 -0800

    remove add aggregate or window function

--- src/optimizer/join_order/relation_manager.cpp
+++ src/optimizer/join_order/relation_manager.cpp
@@ -47,37 +47,49 @@ void RelationManager::AddAggregateOrWindowRelation(LogicalOperator &op, optional
 void RelationManager::AddRelation(LogicalOperator &op, optional_ptr<LogicalOperator> parent,
                                   const RelationStats &stats) {
 
-	// if parent is null, then this is a root relation
-	// if parent is not null, it should have multiple children
-	D_ASSERT(!parent || parent->children.size() >= 2);
+	// // if parent is null, then this is a root relation
+	// // if parent is not null, it should have multiple children
+	// D_ASSERT(!parent || parent->children.size() >= 2);
+	// auto relation = make_uniq<SingleJoinRelation>(op, parent, stats);
+	// auto relation_id = relations.size();
+	//
+	// auto table_indexes = op.GetTableIndex();
+	// if (table_indexes.empty()) {
+	// 	// relation represents a non-reorderable relation, most likely a join relation
+	// 	// Get the tables referenced in the non-reorderable relation and add them to the relation mapping
+	// 	// This should return all table references, even if there are nested non-reorderable joins.
+	// 	unordered_set<idx_t> table_references;
+	// 	LogicalJoin::GetTableReferences(op, table_references);
+	// 	D_ASSERT(table_references.size() > 0);
+	// 	for (auto &reference : table_references) {
+	// 		D_ASSERT(relation_mapping.find(reference) == relation_mapping.end());
+	// 		relation_mapping[reference] = relation_id;
+	// 	}
+	// } else if (op.type == LogicalOperatorType::LOGICAL_UNNEST) {
+	// 	// logical unnest has a logical_unnest index, but other bindings can refer to
+	// 	// columns that are not unnested.
+	// 	auto bindings = op.GetColumnBindings();
+	// 	for (auto &binding : bindings) {
+	// 		relation_mapping[binding.table_index] = relation_id;
+	// 	}
+	// } else {
+	// 	// Relations should never return more than 1 table index
+	// 	D_ASSERT(table_indexes.size() == 1);
+	// 	idx_t table_index = table_indexes.at(0);
+	// 	D_ASSERT(relation_mapping.find(table_index) == relation_mapping.end());
+	// 	relation_mapping[table_index] = relation_id;
+	// }
+	// relations.push_back(std::move(relation));
+	// op.estimated_cardinality = stats.cardinality;
+	// op.has_estimated_cardinality = true;
 	auto relation = make_uniq<SingleJoinRelation>(op, parent, stats);
 	auto relation_id = relations.size();
 
-	auto table_indexes = op.GetTableIndex();
-	if (table_indexes.empty()) {
-		// relation represents a non-reorderable relation, most likely a join relation
-		// Get the tables referenced in the non-reorderable relation and add them to the relation mapping
-		// This should all table references, even if there are nested non-reorderable joins.
-		unordered_set<idx_t> table_references;
-		LogicalJoin::GetTableReferences(op, table_references);
-		D_ASSERT(table_references.size() > 0);
-		for (auto &reference : table_references) {
-			D_ASSERT(relation_mapping.find(reference) == relation_mapping.end());
-			relation_mapping[reference] = relation_id;
-		}
-	} else if (op.type == LogicalOperatorType::LOGICAL_UNNEST) {
-		// logical unnest has a logical_unnest index, but other bindings can refer to
-		// columns that are not unnested.
-		auto bindings = op.GetColumnBindings();
-		for (auto &binding : bindings) {
+	auto op_bindings = op.GetColumnBindings();
+	for (auto &binding : op_bindings) {
+		if (relation_mapping.find(binding.table_index) == relation_mapping.end()) {
 			relation_mapping[binding.table_index] = relation_id;
 		}
-	} else {
-		// Relations should never return more than 1 table index
-		D_ASSERT(table_indexes.size() == 1);
-		idx_t table_index = table_indexes.at(0);
-		D_ASSERT(relation_mapping.find(table_index) == relation_mapping.end());
-		relation_mapping[table_index] = relation_id;
 	}
 	relations.push_back(std::move(relation));
 	op.estimated_cardinality = stats.cardinality;
