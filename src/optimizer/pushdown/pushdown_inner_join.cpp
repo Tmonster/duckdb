81c6b49774e965310cb19d8d92b1e53bd2c230a4
Tmonster <tom@ebergen.com>
2025-06-25 15:11:26 +0200

    add another data point

--- src/optimizer/pushdown/pushdown_inner_join.cpp
+++ src/optimizer/pushdown/pushdown_inner_join.cpp
@@ -15,20 +15,30 @@ unique_ptr<LogicalOperator> FilterPushdown::PushdownInnerJoin(unique_ptr<Logical
 	D_ASSERT(join.join_type == JoinType::INNER);
 	if (op->type == LogicalOperatorType::LOGICAL_DELIM_JOIN) {
 		// try to push the current filters into the children.
+		unordered_set<idx_t> remove_filters;
 		for (idx_t i = 0; i < join.children.size(); i++) {
 			FilterPushdown new_pushdown(optimizer, convert_mark_joins);
 			unordered_set<idx_t> child_bindings;
 			LogicalJoin::GetTableReferences(*op->children[i], child_bindings);
 			// only add filters to the children if we know the bindings are in the join child
-			for (auto &filter : filters) {
-				if (std::all_of(filter->bindings.begin(), filter->bindings.end(),
-				                [&](const idx_t &binding) { return child_bindings.count(binding); })) {
+			for (idx_t j = 0; j < filters.size(); j++) {
+				auto &filter = filters[j];
+				bool contains_all_bindings =
+				    std::all_of(filter->bindings.begin(), filter->bindings.end(),
+				                [&](const idx_t &binding) { return child_bindings.count(binding); });
+				if (contains_all_bindings) {
 					new_pushdown.AddFilter(filter->filter->Copy());
+					remove_filters.insert(j);
 				}
 			}
 			new_pushdown.GenerateFilters();
 			if (!new_pushdown.filters.empty()) {
 				join.children[i] = new_pushdown.Rewrite(std::move(join.children[i]));
+			}
+		}
+		for (auto &pushed_filter : remove_filters) {
+			if (pushed_filter < filters.size()) {
+				filters.erase(filters.begin() + static_cast<int64_t>(pushed_filter));
 			}
 		}
 		// TODO: prevent recursive calls since the Rewrites above will already
